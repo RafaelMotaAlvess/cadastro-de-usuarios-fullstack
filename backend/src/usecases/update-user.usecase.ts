ecfd3bb679a13986be34373f6821b8a6db285995
RafaelMotaAlvess <leafardzn@gmail.com>
2024-02-17 18:56:03 -0300

    feat: create `UpdateUserUseCase`

---
+++ backend/src/usecases/update-user.usecase.ts
@@ -0,0 +1,65 @@
+import {
+  FindByUserEmailRepository,
+  FindByUserPhoneRepository,
+  UpdateUserRepository
+} from "../repositories";
+
+import {
+  EmailAlreadyExistsError,
+  PhoneAlreadyExistsError
+} from "./errors";
+
+import { User } from "../models"
+
+
+export interface UpdateUserUseCaseRequest {
+  name?: string;
+  email?: string;
+  phone?: string;
+}
+
+interface UpdateUserUseCaseResponse {
+  user: User
+}
+
+
+export class UpdateUserUseCase {
+  constructor(
+    private updateUserRepository: UpdateUserRepository,
+    private findByUserEmailRepository: FindByUserEmailRepository,
+    private findByUserPhoneRepository: FindByUserPhoneRepository
+  ) { }
+
+  async execute(id: string, {
+    name,
+    email,
+    phone
+  }: UpdateUserUseCaseRequest): Promise<UpdateUserUseCaseResponse> {
+
+    if (email) {
+      const userWithSameEmail = await this.findByUserEmailRepository.findByEmail(email)
+
+      if (userWithSameEmail) {
+        throw new EmailAlreadyExistsError();
+      }
+    }
+
+    if (phone) {
+      const userWithSamePhone = await this.findByUserPhoneRepository.findByPhone(phone)
+
+      if (userWithSamePhone) {
+        throw new PhoneAlreadyExistsError();
+      }
+    }
+
+    const user = await this.updateUserRepository.update(id, {
+      name,
+      email,
+      phone
+    })
+
+    return {
+      user
+    }
+  }
+}
