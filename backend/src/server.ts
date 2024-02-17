b7e9189e5469c4da0ad49cb98e741ed15b2ccd9e
RafaelMotaAlvess <leafardzn@gmail.com>
2024-02-17 15:56:00 -0300

    feat: initiliaze server

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import { env, app } from "./config";
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`HTTP Server Running ✨ localhost:${env.PORT}`);
+  });
