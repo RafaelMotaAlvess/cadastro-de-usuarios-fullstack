6c3cbfd04846066df751b73ecccbb0016108ab83
RafaelMotaAlvess <leafardzn@gmail.com>
2024-02-17 22:36:03 -0300

    test: should be fetch users order by date (desc)

---
+++ backend/src/usecases/fetch-users-by-created-at-desc.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryUsersRepository } from 'test'
+import { it, beforeEach, describe, expect, vi } from 'vitest'
+import { FetchUsersByCreatedAtDescUseCase } from './fetch-users-by-created-at-desc'
+
+let usersRepository: InMemoryUsersRepository
+let sut: FetchUsersByCreatedAtDescUseCase
+
+describe("Find Users  By Created At Usecase", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new FetchUsersByCreatedAtDescUseCase(usersRepository)
+  })
+
+  it("should be fetch users order by date (desc)", async () => {
+    vi.useFakeTimers();
+
+    const currentTime = new Date().getTime()
+
+    for (let i = 1; i < 3; i++) {
+      const timeOffset = i * (24 * 60 * 60 * 1000) // ! day
+      vi.setSystemTime(currentTime + timeOffset)
+      await usersRepository.create({
+        name: "Rafael Mota Alves",
+        email: `rafaelmotalaves${i}@gmail.com`,
+        phone: `10101010100${i}`
+      });
+    }
+
+    vi.useRealTimers()
+
+    const { users } = await sut.execute()
+
+    expect(users).toHaveLength(2)
+    expect(users[0]).toEqual(expect.objectContaining({ email: "rafaelmotalaves2@gmail.com" }))
+    expect(users[1]).toEqual(expect.objectContaining({ email: "rafaelmotalaves1@gmail.com" }))
+  })
+})
