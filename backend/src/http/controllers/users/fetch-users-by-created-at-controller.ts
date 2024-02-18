5affd79e4198b6c929b3a7705cfc8d781e22df43
RafaelMotaAlvess <leafardzn@gmail.com>
2024-02-18 17:19:37 -0300

    feat: create `fetchUsersByCreatedAt` controller

---
+++ backend/src/http/controllers/users/fetch-users-by-created-at-controller.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { makeFetchUsersByCreatedAtUseCase } from "../../../usecases/factories";
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function fetchUsersByCreatedAt(request: FastifyRequest, reply: FastifyReply) {
+  const fetchUsersByCreatedAtParamsSchema = z.object({
+    date: z.string()
+  })
+
+  try {
+    const { date } = fetchUsersByCreatedAtParamsSchema.parse(request.params);
+
+    const dateToSend = new Date(date);
+
+    const fetchUsersUsecase = makeFetchUsersByCreatedAtUseCase();
+
+    const users = await fetchUsersUsecase.execute({ date: dateToSend });
+
+    return reply.status(200).send(users);
+  } catch (_) {
+    return reply.status(404).send([]);
+  }
+
+}
