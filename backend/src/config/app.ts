4ff0cd605a54351c81fda5d536b4fc9d3b827543
RafaelMotaAlvess <leafardzn@gmail.com>
2024-02-19 02:17:26 -0300

    chore: add fastify `cors`

--- backend/src/config/app.ts
+++ backend/src/config/app.ts
@@ -1,6 +1,13 @@
-import { userRoutes } from "../http/controllers/users/routes";
 import fastify from "fastify";
+import cors from '@fastify/cors'
+
+import { userRoutes } from "../http/controllers/users/routes";
 
 export const app = fastify()
+
+app.register(cors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+})
 
 app.register(userRoutes)
