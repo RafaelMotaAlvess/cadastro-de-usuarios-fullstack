5d8f9d997abcf60bc2b5902a30d1f003ac65e5cb
RafaelMotaAlvess <leafardzn@gmail.com>
2024-02-19 13:50:02 -0300

    feat: add table space

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -114,7 +114,7 @@ export function Users() {
   }
 
   return (
-    <div className="p-6 max-w-5xl mx-auto space-y-4">
+    <div className="p-6 max-w-6xl mx-auto space-y-4">
       <h1 className='text-3xl font-bold'>Usuarios</h1>
 
       <div className="flex flex-wrap items-center justify-center sm:justify-between space-y-2 sm:space-y-0">
